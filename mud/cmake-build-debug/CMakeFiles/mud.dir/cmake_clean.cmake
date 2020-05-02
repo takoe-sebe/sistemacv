c9a358264ac69a936c3801f0fd875b43831f279b
takoe-sebe <masha-masha-efimova@mail.ru>
2020-05-02 21:48:03 +0300

    mud

---
+++ mud/cmake-build-debug/CMakeFiles/mud.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mud.dir/main.cpp.obj"
+  "libmud.dll.a"
+  "mud.exe"
+  "mud.exe.manifest"
+  "mud.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
